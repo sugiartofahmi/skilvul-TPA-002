1bc5594e3685d6496f1882539b90b5a150f250ec
sugiartofahmi <sugiartofahmi@gmail.com>
2023-04-02 22:01:05 +0700

    Fahmi Sugiarto | Add validation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,38 @@
 const button = document.getElementById("form_button"),
-  weight = document.getElementById("input_weigth"),
+  weight = document.getElementById("input_weight"),
   height = document.getElementById("input_height"),
-  output = document.getElementById("output_text");
+  output = document.getElementById("output_text"),
+  validationWeight = document.getElementById("validation_weight"),
+  validationHeight = document.getElementById("validation_height");
 let result = "",
   bmi = "";
 
 button.onclick = () => {
-  result = (weight.value / (height.value / 100) ** 2).toFixed(1);
-  bmi =
-    result <= 18.5
-      ? "Underweight"
-      : result > 18.5 && result <= 24.9
-      ? "Normal weight"
-      : result >= 25 && result <= 29.9
-      ? "Overweight "
-      : result >= 30 && "Obesity";
-  weight.value.length > 0 && height.value.length != 0
-    ? (output.innerHTML =
-        ` <p id="output_text">Your BMI is <strong> ` +
+  weight.value.length < 1
+    ? (validationWeight.innerHTML = `<span class="danger">Weight cannot be empty</span>`)
+    : (validationWeight.innerHTML = ``);
+  height.value.length < 1
+    ? (validationHeight.innerHTML = `<span class="danger">Height cannot be empty</span>`)
+    : (validationHeight.innerHTML = ``);
+
+  weight.value.length > 1 && height.value.length > 1
+    ? ((result = (weight.value / (height.value / 100) ** 2).toFixed(1)),
+      (bmi =
+        result <= 18.5
+          ? "Underweight"
+          : result > 18.5 && result <= 24.9
+          ? "Normal weight"
+          : result >= 25 && result <= 29.9
+          ? "Overweight "
+          : result >= 30 && "Obesity"),
+      (output.innerHTML =
+        ` Your BMI is <strong> ` +
         result +
         `</strong> which means You are
   <strong>` +
         bmi +
-        `</strong></p>`)
-    : (output.innerHTML = `<p class="danger" id="output_text">Input cannot be empty</p>`);
+        `</strong>`),
+      (weight.value = ""),
+      (height.value = ""))
+    : (output.innerHTML = ``);
 };
